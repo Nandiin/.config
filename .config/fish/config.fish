e4ade266fd11bdb93362d60ae2b503ab9d325fb3
Nandiin <nandiin@163.com>
2017-04-19 14:19:39 +0800

    fish_user_path is one-time-set variable, thus not proper for being in start up file. some security-related settings (e.g. tokens) are put inside ~/.local/share/fish/config.fish and not synced.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 alias l "ls -al"
 set -l user_python (python -c "import site; print(site.getuserbase())")
-set -U fish_user_paths "$user_python/bin" $fish_user_paths
+set -x PATH $user_python $PATH
+if test -e ~/.local/share/fish/config.fish
+	source ~/.local/share/fish/config.fish
+end
+
