b380ac4ef78f10072a2d07c25703a91e5e19676c
Nandiin <nandiin@163.com>
2017-04-19 15:09:43 +0800

    fix user python bin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 alias l "ls -al"
 set -l user_python (python -c "import site; print(site.getuserbase())")
-set -x PATH $user_python $PATH
+set -x PATH "$user_python/bin" $PATH
 if test -e ~/.local/share/fish/config.fish
 	source ~/.local/share/fish/config.fish
 end
